61facc2262f91187c3ec52ca92f961263acd403d
Jaheen Reza <85499355+JSReza@users.noreply.github.com>
2026-02-25 08:52:55 -0500

    imported fullcalendar.io and populated the placeholder-data.json with mock data

--- streakeeper/src/App.vue
+++ streakeeper/src/App.vue
@@ -2,11 +2,15 @@
 import {ref} from 'vue'
 import NavBar from './components/NavBar.vue';
 import UserInput from './components/UserInput.vue';
+import StreakData from './components/StreakData.vue';
+import CalendarDisplay from './components/CalendarDisplay.vue';
 
 </script>
 <template>
  <NavBar/>
+ <StreakData/>
  <UserInput/>
+ <CalendarDisplay/>
 </template>
 
 <style scoped></style>
