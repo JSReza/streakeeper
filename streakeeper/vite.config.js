514501c1529cd6860fb5bbb3f195815e0ce395d7
Jaheen Reza <85499355+JSReza@users.noreply.github.com>
2026-03-04 08:25:48 -0500

    made small changes to server.js and vite.config file to  prepare to deploy to github pages

--- streakeeper/vite.config.js
+++ streakeeper/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/streakeeper',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
